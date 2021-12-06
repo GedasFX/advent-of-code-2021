ccfcd308a460b70cb50b91a979295e9346b6c973
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-12-06 22:26:27 +0200

    1/50

---
+++ Day1_1.SonarSweep/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+int prev = -1;
+var count = 0;
+foreach (var line in File.ReadLines("input.txt"))
+{
+    var curr = int.Parse(line);
+
+    if (prev > 0)
+    {
+        count += curr > prev ? 1 : 0;
+    }
+
+    prev = curr;
+}
+
+Console.WriteLine(count);
