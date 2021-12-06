0ed9f72df5d716069043f9b663dfd07632bae4d9
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-12-06 22:47:25 +0200

    Part 2

--- Day1_1.SonarSweep/Program.cs
+++ Day1_1.SonarSweep/Program.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
-int prev = -1;
-var count = 0;
-foreach (var line in File.ReadLines("input.txt"))
+int Part1()
 {
-    var curr = int.Parse(line);
-
-    if (prev > 0)
+    int prev = -1;
+    var count = 0;
+    foreach (var line in File.ReadLines("input.txt"))
     {
-        count += curr > prev ? 1 : 0;
+        var curr = int.Parse(line);
+
+        if (prev > 0)
+        {
+            count += curr > prev ? 1 : 0;
+        }
+
+        prev = curr;
     }
 
-    prev = curr;
+    return count;
 }
 
-Console.WriteLine(count);
+// Console.WriteLine(Part1());
+
+int Part2()
+{
+    var file = File.ReadAllLines("input.txt").Select(int.Parse).ToList();
+
+    var counter = 0;
+    var prev = file.Take(3).Sum();
+    for (int i = 1; i < file.Count - 2; i++)
+    {
+        var curr = file.Skip(i).Take(3).Sum();
+
+        if (curr > prev)
+            counter++;
+
+        prev = curr;
+    }
+
+    return counter;
+}
+
+Console.WriteLine(Part2());
